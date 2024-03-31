a6c02980a151055fb35a1c2eb7225078b34c8291
SuperAppKid <34574973+SuperAPPKid@users.noreply.github.com>
2024-03-31 15:48:00 +0800

    update homebrew script

--- Casks/neovim-nightly.rb
+++ Casks/neovim-nightly.rb
@@ -1,10 +1,14 @@
-cask 'neovim-nightly' do
+cask "neovim-nightly" do
   version :latest
   sha256 :no_check
+  name "Neovim Nightly"
+  homepage "https://neovim.io/"
 
-  url "https://github.com/neovim/neovim/releases/download/nightly/nvim-macos.tar.gz"
-  name 'Neovim Nightly'
-  homepage 'https://neovim.io/'
-
-  binary 'nvim-macos/bin/nvim'
+  if Hardware::CPU.intel?
+    url "https://github.com/neovim/neovim/releases/download/nightly/nvim-macos-x86_64.tar.gz"
+    binary "nvim-macos-x86_64/bin/nvim"
+  elsif Hardware::CPU.arm?
+    url "https://github.com/neovim/neovim/releases/download/nightly/nvim-macos-arm64.tar.gz"
+    binary "nvim-macos-arm64/bin/nvim"
+  end
 end
